335e617bd484ccbad361e333d3e912bab47249e9
wanjads <wanja.de-sombre@t-online.de>
2022-08-13 14:12:57 +0200

    server ready for tabular

--- simple_scenario/main.py
+++ simple_scenario/main.py
@@ -146,7 +146,7 @@ def test(strategy, data, run, no_of_runs):
 
 def main():
 
-    for experiment in ["network"]:  # "non-learning", "risk-neutral", "tabular",
+    for experiment in ["tabular"]:  # "non-learning", "risk-neutral", "tabular", "network"
 
         print(experiment)
 
